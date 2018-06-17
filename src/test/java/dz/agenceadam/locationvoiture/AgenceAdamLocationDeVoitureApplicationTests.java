6151329f03e1974a05f1932037a470ea124db149
aboumaza <ad.boumaza@yahoo.fr>
2018-06-17 13:45:55 +0200

    j

--- src/test/java/dz/agenceadam/locationvoiture/AgenceAdamLocationDeVoitureApplicationTests.java
+++ src/test/java/dz/agenceadam/locationvoiture/AgenceAdamLocationDeVoitureApplicationTests.java
@@ -43,7 +43,8 @@ public class AgenceAdamLocationDeVoitureApplicationTests {
 
 	@Test
 	public void contextLoads() {
-
+        System.out.println("branch dev");
+        System.out.println("branch master");
 	}
 	
 	@Test
