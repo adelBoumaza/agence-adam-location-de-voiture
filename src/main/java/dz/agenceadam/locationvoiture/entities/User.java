b92ce52295567d30eed00fdebcc49f7c60833b23
aboumaza <ad.boumaza@yahoo.fr>
2018-06-17 13:28:05 +0200

    h

--- src/main/java/dz/agenceadam/locationvoiture/entities/User.java
+++ src/main/java/dz/agenceadam/locationvoiture/entities/User.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 
 @Entity
@@ -24,7 +28,7 @@ public class User implements Serializable{
 	private String userName;
 	private String password;
 	
-	@OneToMany
+	@OneToMany(fetch = FetchType.EAGER)
 	private List<Role> roles = new ArrayList<>();
 	public Integer getId() {
 		return id;
