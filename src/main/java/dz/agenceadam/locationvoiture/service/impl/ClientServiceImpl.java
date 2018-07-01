f2ec4e57e33bcebac54c65ffb589886b9d912a02
aboumaza <ad.boumaza@yahoo.fr>
2018-07-01 14:30:02 +0200

    m

--- src/main/java/dz/agenceadam/locationvoiture/service/impl/ClientServiceImpl.java
+++ src/main/java/dz/agenceadam/locationvoiture/service/impl/ClientServiceImpl.java
@@ -36,8 +36,6 @@ public class ClientServiceImpl implements IClientService{
 		return clientRepository.findAllClientActived();
 	}
 	
-//comment 
-	//comment
 	@Override
 	public Client saveClientWidthUser(ClientDto clientDto, Integer idUser) throws ParseException, DataFoundedException{
 		Client client = GenericBuilder.of(Client::new)
