25b5462473558f57b1387bf983ffa523c81293ae
aboumaza <ad.boumaza@yahoo.fr>
2018-12-30 15:13:22 +0100

    new commit path variable

--- src/main/java/dz/agenceadam/locationvoiture/controller/ProfileRest.java
+++ src/main/java/dz/agenceadam/locationvoiture/controller/ProfileRest.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import dz.agenceadam.locationvoiture.dto.ProfileDto;
+import dz.agenceadam.locationvoiture.entities.Profile;
 import dz.agenceadam.locationvoiture.service.IProfileService;
+import dz.agenceadam.locationvoiture.util.GenericBuilder;
 
 @RestController
 @RequestMapping(value="/api/v1")
@@ -28,17 +30,38 @@ public class ProfileRest {
 	@Autowired
 	private IProfileService profileService;
 
-	@PostMapping("/profile/saveOrUpdate/{save}")
-	public ResponseEntity<Object> saveorUpdate(@RequestBody ProfileDto profileDto,
+	@PostMapping("/profile/saveOrUpdate/{nom}/{prenom}/{nomAgence}/{numeTelOne}/{numeTelTwo}/{mail}/{adresse}/{idUser}/{save}")
+	public ResponseEntity<Object> saveorUpdate(
 											   @RequestParam("file") MultipartFile multipartFile,
-			                                   @PathVariable boolean save) throws IOException 
+			                                   @PathVariable String nom,
+			                                   @PathVariable String prenom,
+			                                   @PathVariable String nomAgence,
+			                                   @PathVariable String numeTelOne,
+			                                   @PathVariable String numeTelTwo,
+			                                   @PathVariable String mail,
+			                                   @PathVariable String adresse,
+			                                   @PathVariable Integer idUser,
+			                                   @PathVariable boolean save
+			                                   ) throws IOException 
 	{
 		File convFile = new File(multipartFile.getOriginalFilename());
 	    convFile.createNewFile(); 
 	    //FileOutputStream fos = new FileOutputStream(convFile); 
 	    //fos.write(multipartFile.getBytes());
 	    //fos.close(); 
-	    profileDto.setLogo(multipartFile.getBytes());
+	    ProfileDto profileDto = GenericBuilder.of(ProfileDto::new)
+				.with(ProfileDto::setAdresse, adresse)
+				.with(ProfileDto::setNomAgence, nomAgence)
+				.with(ProfileDto::setNom, nom)
+				.with(ProfileDto::setPrenom, prenom)
+				.with(ProfileDto::setEmail, mail)
+				.with(ProfileDto::setNumeTelOne, numeTelOne)
+				.with(ProfileDto::setNumTelTwo, numeTelTwo)
+				.with(ProfileDto::setActived, Boolean.TRUE)
+				.with(ProfileDto::setIdUser, idUser)
+				.with(ProfileDto::setLogo, multipartFile.getBytes())
+				.with(ProfileDto::setNomFichier, convFile.getName())
+				.build();
 		profileService.saveOrUpdate(profileDto, save);
 		return new ResponseEntity<>("le logo bien chargee",HttpStatus.OK);
 	}
